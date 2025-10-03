9ff62347df7e09c6aea5f6229419b029967f54a9
Porter Iverson <live4soccer02@icloud.com>
2025-10-03 13:14:16 -0600

    fixed authentication bug

--- frontend/src/components/login-form.tsx
+++ frontend/src/components/login-form.tsx
@@ -11,12 +11,14 @@ import { Label } from '../components/ui/label'
 import { useState } from 'react'
 import { supabase } from '../services/supabaseClient'
 import { cn } from '../lib/utils'
+import { useNavigate } from 'react-router-dom'
 
 export function LoginForm({ className, ...props }: React.ComponentPropsWithoutRef<'div'>) {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [error, setError] = useState<string | null>(null)
   const [isLoading, setIsLoading] = useState(false)
+  const navigate = useNavigate()
 
 
   const handleLogin = async (e: React.FormEvent) => {
@@ -30,8 +32,7 @@ export function LoginForm({ className, ...props }: React.ComponentPropsWithoutRe
         password,
       })
       if (error) throw error
-      // Update this route to redirect to an authenticated route. The user already has an active session.
-      location.href = '/dashboard'
+      navigate('/dashboard')
     } catch (error: unknown) {
       setError(error instanceof Error ? error.message : 'An error occurred')
     } finally {
